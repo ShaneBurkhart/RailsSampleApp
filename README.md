5ad69f790a66d2d2f7c91c4f9395d1c8ade6148d
ShaneBurkhart <shaneburkhart@gmail.com>
2013-05-01 23:20:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Static Pages RoR Project
+
+A project to learn rails.  The first of the tutorials, consisting of only static pages.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8ff45a938475ee2f9ce80653b85bebc3392dfd5c44de91af6efeeff0b0b795324b51fd7c9e20e038342c69ea8313d92ad79a2dcb8a3837fb9f5343f11ba74312'
